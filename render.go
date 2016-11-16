96be390960016daca23b1cc2a3cca3803804238a
Raven <enn.raven@gmail.com>
2016-11-16 15:40:28 +0800

    Refactor go doc of render a bit.

--- render.go
+++ render.go
@@ -1,3 +1,4 @@
+// Render support to render templates by your control.
 package render
 
 import (
@@ -6,13 +7,13 @@ import (
 	"path/filepath"
 )
 
-// Render render struct
+// Render the render struct.
 type Render struct {
 	ViewPaths []string
 	funcMaps  template.FuncMap
 }
 
-// New initalize render struct
+// New initalize the render struct.
 func New(viewPaths ...string) *Render {
 	if isExistingDir(filepath.Join(root, "app/views")) {
 		viewPaths = append(viewPaths, filepath.Join(root, "app/views"))
@@ -21,22 +22,22 @@ func New(viewPaths ...string) *Render {
 	return &Render{ViewPaths: viewPaths, funcMaps: map[string]interface{}{}}
 }
 
-// Layout set layout for tmpl
+// Layout set layout for template.
 func (render *Render) Layout(name string) *Template {
 	return &Template{render: render, layout: name}
 }
 
-// Funcs set funcs for tmpl with application layout
+// Funcs set helper functions for template with default "application" layout.
 func (render *Render) Funcs(funcMap template.FuncMap) *Template {
 	return render.Layout("application").Funcs(funcMap)
 }
 
-// Execute execute tmpl with application layout
+// Execute render template with default "application" layout.
 func (render *Render) Execute(name string, context interface{}, request *http.Request, writer http.ResponseWriter) error {
 	return render.Layout("application").Execute(name, context, request, writer)
 }
 
-// RegisterFuncMap register FuncMap for render
+// RegisterFuncMap register FuncMap for render.
 func (render *Render) RegisterFuncMap(name string, fc interface{}) {
 	if render.funcMaps == nil {
 		render.funcMaps = template.FuncMap{}
