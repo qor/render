5b68a63157c32adc360e7bee8c136e93c3049abc
Raven <enn.raven@gmail.com>
2019-06-12 16:26:14 +0800

    Allow configuring correct viewpath in test. especially for test located in deep directory

--- render.go
+++ render.go
@@ -68,6 +68,9 @@ func (render *Render) RegisterViewPath(paths ...string) {
 				render.AssetFileSystem.RegisterPath(absPath)
 			} else if isExistingDir(filepath.Join(utils.AppRoot, "vendor", pth)) {
 				render.AssetFileSystem.RegisterPath(filepath.Join(utils.AppRoot, "vendor", pth))
+			} else if p := filepath.Join(utils.AppRoot, pth); isExistingDir(p) { // This allows configuring correct viewpath in test. especially for test located in deep directory
+				render.ViewPaths = append([]string{p}, render.ViewPaths...)
+				render.AssetFileSystem.PrependPath(filepath.Join(utils.AppRoot, pth))
 			} else {
 				for _, gopath := range utils.GOPATH() {
 					if p := filepath.Join(gopath, "src", pth); isExistingDir(p) {
